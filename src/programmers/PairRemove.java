dac3366f43de3edf5ebd6218d1823bf880229aed
onAuspicious <wotj7687@icloud.com>
2021-07-12 21:59:03 +0900

    add Programmers solution 짝지어 제거하기

---
+++ src/programmers/PairRemove.java
@@ -0,0 +1,31 @@
+package programmers;
+
+import java.util.Stack;
+
+public class PairRemove {
+    public int solution(String s)
+    {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (stack.isEmpty()) {
+                stack.add(s.charAt(i));
+            } else if (s.charAt(i) == stack.lastElement()) {
+                stack.pop();
+            } else {
+                stack.add(s.charAt(i));
+            }
+        }
+        if (stack.isEmpty()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "baabaa";
+        PairRemove pairRemove = new PairRemove();
+        System.out.println(pairRemove.solution(s));
+    }
+}
