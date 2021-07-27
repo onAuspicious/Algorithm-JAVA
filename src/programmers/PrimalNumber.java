bc6c5f5e619010131a049b101eaeea0b620ec24f
onAuspicious <wotj7687@icloud.com>
2021-07-27 22:27:58 +0900

    add Solution:  소수찾기

---
+++ src/programmers/PrimalNumber.java
@@ -0,0 +1,36 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PrimalNumber {
+
+    public ArrayList<Integer> findPriority() {
+        int[] memo = new int[1001];
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int i = 2; i < 1001; i++) {
+            if (memo[i] == 0) {
+                for (int j = 1; j <1000/i+1; j++) {
+                    memo[j*i] = 1;
+                }
+                result.add(i);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> priority = new PrimalNumber().findPriority();
+        int N = scanner.nextInt();
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            int num = scanner.nextInt();
+            if (priority.contains(num)) {
+                result += 1;
+            }
+        }
+        System.out.println(result);
+    }
+}
