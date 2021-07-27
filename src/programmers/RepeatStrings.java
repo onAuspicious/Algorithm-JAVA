c4b0af64dede2e7f10dee4516670f7371212e9ac
onAuspicious <wotj7687@icloud.com>
2021-07-28 03:16:57 +0900

    add Solution: 문자열 반복

---
+++ src/programmers/RepeatStrings.java
@@ -0,0 +1,22 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class RepeatStrings {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < N; i++) {
+            String[] next = scanner.nextLine().split(" ");
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int j = 0; j < next[1].length(); j++) {
+                for (int k = 0; k < Integer.parseInt(next[0]); k++) {
+                    stringBuilder.append(next[1].charAt(j));
+                }
+            }
+            System.out.println(stringBuilder.toString());
+        }
+    }
+}
