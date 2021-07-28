31ac079d480523377d2466b5ed8bf47e3013d284
onAuspicious <wotj7687@icloud.com>
2021-07-28 16:39:28 +0900

    add Solution: 구구단

---
+++ src/programmers/Gugudan.java
@@ -0,0 +1,14 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class Gugudan {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        for (int i = 1; i < 10; i++) {
+            System.out.println(N + " * " + i + " = " + N*i);
+        }
+    }
+}
