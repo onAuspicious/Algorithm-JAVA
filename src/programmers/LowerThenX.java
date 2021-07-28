d2461cb52fa386e8b03c15a651fa88ab47e013d4
onAuspicious <wotj7687@icloud.com>
2021-07-28 22:13:28 +0900

    add Solution: X 보다 작은 수

---
+++ src/programmers/LowerThenX.java
@@ -0,0 +1,21 @@
+package programmers;
+
+import java.io.*;
+
+public class LowerThenX {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] NX = br.readLine().split(" ");
+        int N = Integer.parseInt(NX[0]);
+        int X = Integer.parseInt(NX[1]);
+        String[] list = br.readLine().split(" ");
+        for (String s : list) {
+            if (Integer.parseInt(s) < X) {
+                bw.write(s + " ");
+            }
+        }
+        bw.close();
+    }
+}
