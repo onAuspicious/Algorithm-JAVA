a0e450836fa6afc88a574402c3d7cf8494257d4d
onAuspicious <wotj7687@icloud.com>
2021-07-29 21:27:08 +0900

    add Solution: 상수

---
+++ src/programmers/SangSoo.java
@@ -0,0 +1,19 @@
+package programmers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SangSoo {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] list = br.readLine().split(" ");
+
+        StringBuilder first = new StringBuilder(list[0]);
+        StringBuilder second = new StringBuilder(list[1]);
+        int f = Integer.parseInt(first.reverse().toString());
+        int s = Integer.parseInt(second.reverse().toString());
+        System.out.println(Math.max(f,s));
+    }
+}
