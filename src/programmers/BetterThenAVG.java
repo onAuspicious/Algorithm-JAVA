d447b33263c3012fa5643693de7594bbc5edbfcb
onAuspicious <wotj7687@icloud.com>
2021-07-29 18:10:11 +0900

    add Solution: 평균은 넘겠지

---
+++ src/programmers/BetterThenAVG.java
@@ -0,0 +1,41 @@
+package programmers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BetterThenAVG {
+
+    public void solution(String[] list) {
+        int num = Integer.parseInt(list[0]);
+        double avg = 0D;
+        double result = 0D;
+
+        for (int i = 1; i < list.length; i++) {
+            avg += Double.parseDouble(list[i]);
+        }
+
+        avg /= num;
+
+        for (int i = 1; i < list.length; i++) {
+            if (Double.parseDouble(list[i]) > avg) {
+                result += 1;
+            }
+        }
+
+        result /= num;
+        System.out.println(String.format("%.3f", result*100) + "%");
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            String[] list = br.readLine().split(" ");
+            BetterThenAVG betterThenAVG = new BetterThenAVG();
+            betterThenAVG.solution(list);
+        }
+        br.close();
+    }
+}
