2f4a51784ba9f3c838e2e2f5f75806fb54c3f73d
onAuspicious <wotj7687@icloud.com>
2021-08-01 20:52:32 +0900

    add Solution: 분수찾기

---
+++ src/programmers/FractionSearch.java
@@ -0,0 +1,30 @@
+package programmers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FractionSearch {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int now = 0;
+        int cnt = 1;
+        int left;
+        int right;
+
+        while (now + cnt < N) {
+            now += cnt;
+            cnt += 1;
+        }
+        if (cnt % 2 == 0) {
+            left = N - now;
+            right = cnt - N + now + 1;
+        } else {
+            left = cnt - N + now + 1;
+            right = N - now;
+        }
+        System.out.println(left + "/" + right);
+    }
+}
