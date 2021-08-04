24298750b0b22093d489cd929db17dbf253211f4
onAuspicious <wotj7687@icloud.com>
2021-08-04 15:39:05 +0900

    add solution : 수 정렬하기1,2

---
+++ src/problems/NumberSorting.java
@@ -0,0 +1,26 @@
+package problems;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class NumberSorting {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+        Collections.sort(list);
+        for (Integer integer : list) {
+            sb.append(integer).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
