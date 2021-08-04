de79927e9956aef3fc60d386fea72febabded726
onAuspicious <wotj7687@icloud.com>
2021-08-04 22:22:57 +0900

    modify solution : 단어 정렬

--- src/problems/WordSort.java
+++ src/problems/WordSort.java
@@ -26,12 +26,7 @@ public class WordSort {
             @Override
             public int compare(String o1, String o2) {
                 if (o1.length() == o2.length()) {
-                    for (int i = 0; i < o1.length(); i++) {
-                        if (o1.charAt(i) != o2.charAt(i)) {
-                            return o1.charAt(i) - o2.charAt(i);
-                        }
-                    }
-                    return 0;
+                    return o1.compareTo(o2);
                 } else {
                     return o1.length() - o2.length();
                 }
