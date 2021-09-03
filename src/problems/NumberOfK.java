0991bb1cac09080bd098bc68d0b4d52f86396195
onAuspicious <wotj7687@icloud.com>
2021-09-03 23:49:51 +0900

    add solution : K 번째 수

---
+++ src/problems/NumberOfK.java
@@ -0,0 +1,20 @@
+package problems;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NumberOfK {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int k = Integer.parseInt(br.readLine());
+        int rt = n * n;
+        int lt = 1;
+
+        while (lt <= rt) {
+            
+        }
+    }
+}
