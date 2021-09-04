0ef79ad8bb090c5de60e0eb1e36e1fb834ad05ed
onAuspicious <wotj7687@icloud.com>
2021-09-04 14:11:11 +0900

    modify solution : K 번째 수

--- src/problems/NumberOfK.java
+++ src/problems/NumberOfK.java
@@ -8,13 +8,27 @@ public class NumberOfK {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int k = Integer.parseInt(br.readLine());
-        int rt = n * n;
-        int lt = 1;
+        long n = Long.parseLong(br.readLine());
+        long k = Long.parseLong(br.readLine());
+        long rt = n;
+        long lt = 1;
+        long cnt;
+        long mid;
+        long result = 0;
 
         while (lt <= rt) {
-            
+            cnt = 0;
+            mid = (lt + rt) / 2;
+            for (int i = 1; i < n + 1; i++) {
+                cnt += Math.min(mid / i, n);
+            }
+            if (cnt < k) {
+                lt = mid + 1;
+            } else {
+                result = mid;
+                rt = mid - 1;
+            }
         }
+        System.out.println(result);
     }
 }
