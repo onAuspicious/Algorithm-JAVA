fb844e4ae269a07e206137b546d02027821368fa
onAuspicious <wotj7687@icloud.com>
2021-10-05 14:55:03 +0900

    modify solution : 트리의 순회

--- src/problems/TreeSearch_2263.java
+++ src/problems/TreeSearch_2263.java
@@ -1,62 +1,48 @@
 package problems;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class TreeSearch_2263 {
 
-    static int n;
-    static int[] in, post, pre;
-    static int idx;
+    static StringBuilder sb = new StringBuilder();
+    static int[] inorder;
+    static int[] postorder;
 
-    public static void main(String[] args) throws Exception {
-
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        inorder = new int[n];
+        postorder = new int[n];
+        String[] io = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            inorder[i] = Integer.parseInt(io[i]);
+        }
 
-        n = Integer.parseInt(br.readLine());
-
-        in = new int[n];
-        post = new int[n];
-        pre = new int[n];
-
-        String[] sarr = br.readLine().split(" ");
-        for (int i = 0; i < n; i++)
-            in[i] = Integer.parseInt(sarr[i]);
-
-        sarr = br.readLine().split(" ");
-        for (int i = 0; i < n; i++)
-            post[i] = Integer.parseInt(sarr[i]);
-
-        getPreOrder(0, n - 1, 0, n - 1);
-
-        for (int n : pre)
-            bw.write(n + " ");
-
-        bw.flush();
-
+        String[] po = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            postorder[i] = Integer.parseInt(po[i]);
+        }
+        preorder(0, n - 1, 0, n - 1);
+        System.out.println(sb);
     }
 
-    public static void getPreOrder(int is, int ie, int ps, int pe) {
-
-        // is는 인오더 범위의 시작 위치, ie는 인오더의 끝 위치
-        // ps는 포스트오더 범위의 시작 위치, pe는 끝위치
-
+    public static void preorder(int is, int ie, int ps, int pe) {
         if (is <= ie && ps <= pe) {
-
-            pre[idx++] = post[pe]; // 포스트 오더의 가장 오른쪽은(post[pe]) 루트 노드이다.
+            sb.append(postorder[pe]).append(" ");
 
             int pos = is;
-            for (int i = is; i <= ie; i++) { // 인오더에서 루트 노드의 위치를 찾음
-                if (in[i] == post[pe]) {
+            for (int i = is; i < ie; i++) {
+                if (inorder[i] == postorder[pe]) {
                     pos = i;
                     break;
                 }
             }
 
-            // 왼쪽 자식 트리를 가지고 다시 똑같은 과정을 반복한다.
-            getPreOrder(is, pos - 1, ps, ps + pos - is - 1);
-            // 오른쪽 자식 트리를 가지고 다시 똑같은 과정을 반복한다.
-            getPreOrder(pos + 1, ie, ps + pos - is, pe - 1);
+            preorder(is, pos - 1, ps, ps + pos - is - 1);
+            preorder(pos + 1, ie, ps + pos - is, pe - 1);
+
         }
     }
 }
