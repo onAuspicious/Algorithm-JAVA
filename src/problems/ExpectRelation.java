1e3e6aa931d70cf2c3c96c2d220b226a82d8f19a
onAuspicious <wotj7687@icloud.com>
2021-10-21 20:54:15 +0900

    add solution : 예상 대진표

---
+++ src/problems/ExpectRelation.java
@@ -0,0 +1,14 @@
+package problems;
+
+public class ExpectRelation {
+    public int solution(int n, int a, int b)
+    {
+        int result = 0;
+        while(a != b) {
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+            result++;
+        }
+        return result;
+    }
+}
