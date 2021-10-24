bce6f44ef7a1f6d4e1290a85ab175b74f3e20d01
onAuspicious <wotj7687@icloud.com>
2021-10-24 17:16:06 +0900

    add solution : 쉬운 계단 수

---
+++ src/problems/EasyStairNumber.java
@@ -0,0 +1,38 @@
+package problems;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class EasyStairNumber {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        long[][] dp = new long[n + 1][10];
+
+        for (int i = 1; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (j == 0) {
+                    dp[i][j] = dp[i - 1][1];
+                } else if (j == 9) {
+                    dp[i][j] = dp[i - 1][8];
+                } else {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1])%1000000000;
+                }
+            }
+        }
+
+        long result = 0;
+
+        for (int i = 0; i < 10; i++) {
+            result = (result + dp[n][i]) % 1000000000;
+        }
+        System.out.println(result);
+    }
+}
