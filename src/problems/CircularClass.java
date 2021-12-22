a3946f307645589e1cce320390fe63577c3c837a
onAuspicious <wotj7687@icloud.com>
2021-12-22 14:57:03 +0900

    commit solution : 순회강연 (그리디)

---
+++ src/problems/CircularClass.java
@@ -0,0 +1,60 @@
+package problems;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class CircularClass {
+
+    static class Pay{
+        int pay;
+        int day;
+
+        public Pay(int pay, int day) {
+            this.pay = pay;
+            this.day = day;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()); // college
+        Pay[] info = new Pay[n];
+
+        for (int i = 0; i < n; i++) {
+            String[] input = br.readLine().split(" ");
+            int pay = Integer.parseInt(input[0]);
+            int day = Integer.parseInt(input[1]);
+            info[i] = new Pay(pay, day);
+        }
+
+        Arrays.sort(info, Comparator.comparingInt(o -> o.day));
+
+        int cur = 0, benefit = 0;
+        PriorityQueue<Pay> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.pay));
+
+        for (int day = 1; day <= 10000; day++) {
+            while (cur < n && info[cur].day <= day) {
+                if (pq.size() < day) {
+                    pq.offer(info[cur]);
+                } else {
+                    if (pq.peek().pay < info[cur].pay) {
+                        pq.poll();
+                        pq.offer(info[cur]);
+                    }
+                }
+                cur++;
+            }
+        }
+
+        while (!pq.isEmpty()) {
+            benefit += pq.poll().pay;
+        }
+
+        System.out.println(benefit);
+        br.close();
+    }
+}
