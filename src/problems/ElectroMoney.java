3ab41de9e9460027070e7518cb12a889dfc3895d
onAuspicious <wotj7687@icloud.com>
2022-01-12 00:50:27 +0900

    modify solution : ElectroMoney.java

--- src/problems/ElectroMoney.java
+++ src/problems/ElectroMoney.java
@@ -5,23 +5,23 @@ public class ElectroMoney {
     public int solution(int[][] fees, int usage) {
         int amount = 0;
         int base = 0; // 기본 요금
-        int beforeBaseMoney = 0;
+        int beforeBaseUse = 0; // 이전 구간
 
         for (int i = 0; i < fees.length - 1; i++) {
             if (usage > fees[i][0]) {
-                amount += (fees[i][0] - beforeBaseMoney) * fees[i][2];
+                amount += (fees[i][0] - beforeBaseUse) * fees[i][2];
             } else {
-                amount += (usage - beforeBaseMoney) * fees[i][2];
+                amount += (usage - beforeBaseUse) * fees[i][2];
                 base = fees[i][1];
                 break;
             }
-            beforeBaseMoney = fees[i][0];
+            beforeBaseUse = fees[i][0];
         }
 
         // 기본 요금이 있으면 추가 후 리턴, 없으면 마지막 까지 기본요금이 없으므로 계산해서 리턴
         if (base == 0) {
             amount += fees[fees.length - 1][1];
-            amount += (usage - beforeBaseMoney) * fees[fees.length - 1][2];
+            amount += (usage - beforeBaseUse) * fees[fees.length - 1][2];
         } else {
             amount += base;
         }
