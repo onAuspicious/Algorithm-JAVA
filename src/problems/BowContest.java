c8dd980be3af4fc42e3a3f92c7305eb384a17ec5
onAuspicious <wotj7687@icloud.com>
2022-01-14 18:39:15 +0900

    commit solution : Programmers 양궁대회 (DFS)

---
+++ src/problems/BowContest.java
@@ -0,0 +1,48 @@
+package problems;
+
+public class BowContest {
+
+    int maxPoint = 0;
+    int[] result = new int[11];
+
+    public int[] solution(int n, int[] info) {
+
+        DFS(0, info, new int[11], n);
+
+        if (maxPoint == 0) {
+            return new int[]{-1};
+        }
+        return result;
+    }
+
+    public void DFS(int level, int[] info, int[] player, int n) {
+        if (n == 0 || level == 11) {
+            int diff = getDiff(info, player);
+            if (diff >= maxPoint) {
+                maxPoint = diff;
+                System.arraycopy(player, 0, result, 0, 11);
+            }
+            return;
+        }
+
+        if (info[level] < n) {
+            player[level] = info[level] + 1;
+            DFS(level + 1, info, player, n - player[level]);
+            player[level] = 0;
+        }
+        DFS(level + 1, info, player, n);
+    }
+
+
+    public int getDiff(int[] info, int[] player) {
+        int dif = 0;
+        for (int i = 0; i <= 10; i++) {
+            if (player[i] > info[i]) {
+                dif += 10 - i;
+            } else {
+                dif -= 10 - i;
+            }
+        }
+        return dif;
+    }
+}
